9b5433a39f61ee98cb5c862d886be88645713bcc
Shuyi <Shuyi.at.uu@gmail.com>
2017-03-14 10:15:14 +0100

    instantiation of two menus

--- src/MealList.java
+++ src/MealList.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class MealList {
-	private List<Meal> meals;
+	private List<Meal> economyMenu = new ArrayList<>();
+	private List<Meal> vipMenu = new ArrayList<>();
+
 }
